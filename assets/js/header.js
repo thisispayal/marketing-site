7a6d8ba89ab6007ebd0836069a8e1d4dc5835e6a
Daniel Segal <DSegal92@gmail.com>
2013-07-25 12:23:06 -0400

    Adding hash pushing

--- assets/js/header.js
+++ assets/js/header.js
@@ -1,5 +1,8 @@
+var set = null;
+
 $(window).scroll( function() {
    var value = $(this).scrollTop();
+   
 //    if (value > 120){
 //    		$('#vigilant').hide();
 //    		$('#imageshare').show();
@@ -10,30 +13,54 @@ $(window).scroll( function() {
 //    	$('#vigilant').show();
 //    }
 
+	function setHash(value){
+		set = value;
+		var r=value;
+			if(navigator.userAgent.indexOf('Chrome/')!=-1){
+ 			top.history.pushState("", "", r);
+ 			return;
+				};
+		if(r.charAt(0)=='/'){
+ 			top.location.replace(r);
+ 		}
+ 		else{
+  		top.location.hash=r;
+		};
+
+	}
+
    	if (value <= 404){
+   		set = 0;
    		unSelect();
    	}
-    if (value > 404 && value <= 1361){   		
+    if (value > 404 && value <= 1361 && set != "uses"){ 
+    	setHash("uses");
    		unSelect();
-   		$('#uses_link').css('border-bottom', '5px solid white')   		
+   		$('#uses_link').css('border-bottom', '5px solid white')   
+   		
 	}
-	 if (value > 1361 && value <= 1931){   		
+	 if (value > 1361 && value <= 1931 & set!= "features"){  
+	 	setHash("features");	
    		unSelect();
    		$('#features_link').css('border-bottom', '5px solid white')   		
 	}
-	if (value > 1931 && value <= 2785){
+	if (value > 1931 && value <= 2785 & set != "support"){
+		set = "support";
 		unSelect();
 		$('#support_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 2785 && value <= 3668){
+	if (value > 2785 && value <= 3668 && set != "pricing"){
+		set = "pricing;"
 		unSelect();
 		$('#pricing_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 3668 && value <= 4440){
+	if (value > 3668 && value <= 4440 && set != "about_us"){
+		set = "about_us";
 		unSelect();
 		$('#about_us_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 4440){
+	if (value > 4440 && set != "contact"){
+		set = "contact";
 		unSelect();
 		$('#contact_link').css('border-bottom', '5px solid white')
 	}
