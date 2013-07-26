4b6528f4cae902c921358550801ede48ec6f9ffe
Daniel Segal <DSegal92@gmail.com>
2013-07-26 12:28:51 -0400

    Cleaning up CSS

--- assets/js/header.js
+++ assets/js/header.js
@@ -31,37 +31,37 @@ else if (value < 120 && header_image != "vigilant"){
 
 	}
 
-   	if (value <= 404){
-   		set = 0;
+   	if (value <= 1391){
+   		set = null;
    		unSelect();
    	}
-    if (value > 404 && value <= 1361 && set != "uses"){ 
+    if (value > 1391 && value <= 2390 && set != "uses"){ 
     	setHash("uses");
    		unSelect();
    		$('#uses_link').css('border-bottom', '5px solid white')   
    		
 	}
-	 if (value > 1361 && value <= 1931 & set!= "features"){  
+	 if (value > 2390 && value <= 3000 & set!= "features"){  
 	 	setHash("features");	
    		unSelect();
    		$('#features_link').css('border-bottom', '5px solid white')   		
 	}
-	if (value > 1931 && value <= 2785 & set != "support"){
+	if (value > 3000 && value <= 3750 & set != "support"){
 		setHash("support");
 		unSelect();
 		$('#support_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 2785 && value <= 3668 && set != "pricing"){
+	if (value > 3750 && value <= 4600 && set != "pricing"){
 		setHash("pricing");
 		unSelect();
 		$('#pricing_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 3668 && value <= 4440 && set != "about_us"){
+	if (value > 4600 && value <= 5400 && set != "about_us"){
 		setHash("about_us");
 		unSelect();
 		$('#about_us_link').css('border-bottom', '5px solid white')
 	}
-	if (value > 4440 && set != "contact"){
+	if (value > 5400 && set != "contact"){
 		setHash("contact");
 		unSelect();
 		$('#contact_link').css('border-bottom', '5px solid white')
