5df6aa071b3105889196a7bbf6628e95fc8d85c3
thisispayal <patnaikpayal@gmail.com>
2014-02-28 15:54:31 -0500

    Added testimonials/imageshare_user.html

--- assets/js/ga.js
+++ assets/js/ga.js
@@ -1,5 +1,5 @@
   var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-18721456-1']);
+  _gaq.push(['_setAccount', 'UA-18721456-2']);
   _gaq.push(['_setDomainName', 'sharemedicalimages.com']);
   _gaq.push(['_trackPageview']);
 
